a3677e451df2d416730827dbe9685614220dd30b
Anwesh43 <anweshthecool0@gmail.com>
2017-10-02 20:40:47 +0530

    adding navbar

--- page-scroll-block-component.js
+++ page-scroll-block-component.js
@@ -3,8 +3,10 @@ class PageScrollBlockComponent extends HTMLElement {
     constructor() {
         super()
         this.shadow = this.attachShadow({mode:'open'})
+        this.createNavBar()
+        this.createBlocksFromChildren()
     }
-    connectedCallback() {
+    createBlocksFromChildren() {
         for(var i=0;i<this.children.length;i++) {
             const child = this.children[i]
             const childTag = child.tagName.toLowerCase()
@@ -13,6 +15,9 @@ class PageScrollBlockComponent extends HTMLElement {
             }
         }
     }
+    connectedCallback() {
+
+    }
     createDivBlock(block) {
         const div = document.createElement('div')
         div.style.background = block.getAttribute('color') || defaultColor
@@ -20,6 +25,18 @@ class PageScrollBlockComponent extends HTMLElement {
         div.style.height = '100%'
         div.innerHTML = block.innerHTML
         this.shadow.appendChild(div)
+    }
+    createNavBar() {
+        this.bar = document.createElement('div')
+        this.bar.style.position = 'fixed'
+        this.bar.style.background = 'white'
+        this.bar.style.width = '100%'
+        this.bar.style.height = '10%'
+        this.bar.style.top = '0px'
+        this.shadow.appendChild(this.bar)
+    }
+    createNavBarList() {
+
     }
 }
 customElements.define('page-scroll-block',PageScrollBlockComponent)
